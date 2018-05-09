caab81ceeb87c35d349fab1fd02b3f2b7505c5a5
Johan Lundgren <lundgren.johan@yahoo.se>
2018-05-09 15:29:03 +0200

    missed some stuff

---
+++ official/config/page-config.php
@@ -0,0 +1,10 @@
+<?php
+define("START", "<!DOCTYPE html><html lang='sv'><head>");
+
+define("INCLUDES", "<link rel='stylesheet' href='bootstrap/bootstrap.min.css'>");
+
+define("BODY", "</head><body>");
+
+define("JS", "<script src='javascript/jquery-3.2.1.js'></script><script src='bootstrap/bootstrap.min.js'></script>");
+
+define("END", "</body></html>");
