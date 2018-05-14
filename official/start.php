4701b6ad3ab7e71135fe91301f088ffad136fe89
Johan Lundgren <lundgren.johan@yahoo.se>
2018-05-14 18:43:00 +0200

    nav-bar added

--- official/start.php
+++ official/start.php
@@ -6,9 +6,11 @@ echo START;
 echo "<title>Fryksdalens Fårfarm - start</title>";
 
 echo INCLUDES;
+echo "<link rel='stylesheet' href='css/nav-bar.css'>";
 
 echo BODY;
 
+require_once("content/nav-bar.html");
 require_once("content/start.html");
 
 echo JS;
